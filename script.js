91f329767e6a3d6884c95aa8753fbc3a612e72e1
Austin <103972153+Samichpower@users.noreply.github.com>
2023-11-05 15:56:49 -0600

    Add variable for interval and tidy code

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 const allNoteNames = ['A0', 'A#0', 'B0', 'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1', 'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2', 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5', 'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6', 'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7', 'C8'];
 const allIntervals = ['unison', 'min2', 'maj2', 'min3', 'maj3', 'per4', 'tritone', 'per5', 'min6', 'maj6', 'min7', 'maj7', 'octave'];
 
-const newIntervalBtn = document.getElementById('hear-new-btn');
-newIntervalBtn.addEventListener('click', () => {
-  getNextInterval();
-  playNotes(750);
-});
-
-const repeatIntervalBtn = document.getElementById('hear-again-btn');
-repeatIntervalBtn.addEventListener('click', () => {
-  playNotes(750);
-})
-
-
 let rootNoteIndex;
 let intervalNoteIndex;
 let rootNote;
 let intervalNote;
+let selectedIntervals;
+let interval;
 
 function resetAudioPlayback() {
   if (!rootNote) return;
@@ -35,12 +25,23 @@ function getNextInterval() {
     return note;
   }
 
+  function getIntervalNoteIndex() {
+    const randomInterval = Math.floor(Math.random() * selectedIntervals.length);
+    interval = selectedIntervals[randomInterval];
+    console.log(allNoteNames[rootNoteIndex]);
+    console.log(interval);
+    for (let i = 0; i < allIntervals.length; i++) {
+      if (allIntervals[i] === interval) {
+        return rootNoteIndex + i;
+      }
+    }
+  }
+
   rootNoteIndex = Math.floor(Math.random() * (allNoteNames.length - 12));
   rootNote = getAudioFromIndex(rootNoteIndex);
   intervalNoteIndex = getIntervalNoteIndex();
   intervalNote = getAudioFromIndex(intervalNoteIndex);
 }
-
 
 function playNotes(noteTiming) {
   resetAudioPlayback();
@@ -50,9 +51,6 @@ function playNotes(noteTiming) {
     intervalNote.play();
   }, noteTiming);
 }
-
-
-let selectedIntervals;
 
 const newGameBtn = document.getElementById('new-game-btn');
 newGameBtn.addEventListener('click', () => {
@@ -66,13 +64,13 @@ newGameBtn.addEventListener('click', () => {
   console.log(selectedIntervals);
 })
 
-function getIntervalNoteIndex() {
-  const randomInterval = Math.floor(Math.random() * selectedIntervals.length);
-  console.log(allNoteNames[rootNoteIndex]);
-  console.log(selectedIntervals[randomInterval]);
-  for (let i = 0; i < allIntervals.length; i++) {
-    if (allIntervals[i] === selectedIntervals[randomInterval]) {
-      return rootNoteIndex + i;
-    }
-  }
-}
+const repeatIntervalBtn = document.getElementById('hear-again-btn');
+repeatIntervalBtn.addEventListener('click', () => {
+  playNotes(750);
+})
+
+const newIntervalBtn = document.getElementById('hear-new-btn');
+newIntervalBtn.addEventListener('click', () => {
+  getNextInterval();
+  playNotes(750);
+});
