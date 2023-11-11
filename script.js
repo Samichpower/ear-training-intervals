e651634e78f97dbc50f46ba9054553787f3cdf4d
Austin <103972153+Samichpower@users.noreply.github.com>
2023-11-11 12:42:47 -0600

    Link score tracker to object

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const allNoteNames = ['A0', 'A#0', 'B0', 'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1', 'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2', 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5', 'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6', 'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7', 'C8'];
 const allIntervals = ['unison', 'min2', 'maj2', 'min3', 'maj3', 'per4', 'tritone', 'per5', 'min6', 'maj6', 'min7', 'maj7', 'octave'];
 
+const intervalActiveState = {
+  rootNote: null,
+  currentInterval: null,
+  scoreCorrect: 0,
+  scoreTotal: 0,
+  isGameStarted: false,
+  isAnswered: false,
+}
 
 let rootNoteIndex;
 let intervalNoteIndex;
@@ -90,16 +98,16 @@ startGameBtn.addEventListener('click', () => {
   function appendIntervalButtons() {
     function appendScores() {
       scoreCorrectDisplay.forEach((score) => {
-        score.innerHTML = scoreCorrect;
+        score.innerHTML = intervalActiveState.scoreCorrect;
       });
       scoreTotalDisplay.forEach((score) => {
-        score.innerHTML = scoreTotal;
+        score.innerHTML = intervalActiveState.scoreTotal;
       });
     }
 
     function getPercentage() {
+      let percent = Math.round((intervalActiveState.scoreCorrect / intervalActiveState.scoreTotal) * 100);
       percentDisplay.innerHTML = '';
-      let percent = Math.round((scoreCorrect / scoreTotal) * 100);
       percentDisplay.innerHTML = percent;
     }
     
@@ -112,19 +120,19 @@ startGameBtn.addEventListener('click', () => {
       intervalButton.textContent = selectedIntervals[i];
       intervalAnswerContainer.appendChild(intervalButton);
 
-      intervalButton.addEventListener('click', (e) => {
-        if (e.target.id === interval && !isAnswered) {
+      intervalButton.addEventListener('click', (btn) => {
+        if (btn.target.id === interval && !isAnswered) {
           newIntervalBtn.disabled = false;
-          ++scoreCorrect;
-          ++scoreTotal;
+          intervalActiveState.scoreCorrect += 1;
+          intervalActiveState.scoreTotal += 1;
           appendScores();
-        } else if (e.target.id !== interval && !isAnswered) {
-          ++scoreTotal;
+        } else if (btn.target.id !== interval && !isAnswered) {
+          intervalActiveState.scoreTotal += 1;
           appendScores();
-        } else if (e.target.id === interval) {
+        } else if (btn.target.id === interval) {
           newIntervalBtn.disabled = false;
         }
-        e.target.disabled = true;
+        btn.target.disabled = true;
         isAnswered = true;
         getPercentage();
       })
